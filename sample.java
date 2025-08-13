abbb857f5d76bcace7a455efd5e9ca7cedf13c10
jonnalaprasanth <prasanthjonnala4@gmail.com>
2025-08-13 22:48:10 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,16 @@
+class Main
+{
+	public static void main(String arg[])
+	{
+		String name="Jonnala Prasanth";
+		int age=21;
+		String father="J.Siva Satya Narayan(late)";
+		String mother="J.Anjali";
+		String sister=" J.Veera Navya";
+		System.out.println("Name="+name);
+                System.out.println("Prasanth's Age="+age);
+		System.out.println("Father_Name="+father);
+		System.out.println("Mother_Name="+mother);
+		System.out.println("Sister_Name="+sister);
+	}
+}
